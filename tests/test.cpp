e63643aa199416e1e5b5334d5ab73b94a200904d
AvirukBasak <avirukbasak@gmail.com>
2022-10-23 18:26:49 +0530

    na

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,15 +3,15 @@
 
 int main()
 {
-    /* 1st row -> string col names
-     * 1st col -> numeric id, otherwise csv::data::row() returns row based on total number of rows
+    /* 1st row -> col names
+     * 1st col -> slno or uid
      * slno, names,     id,   marks
-     *   5,   John,      i00,  83
-     *   3,   Jack,      i01,  73
+     *   0,   John,      i00,  83
+     *   1,   Jack,      i01,  73
      *   2,   James,     i02,  96
-     *   1,   Johnathan, i03,  79
+     *   3,   Johnathan, i03,  79
      *   4,   Joseph,    i04,  84
-     *   0,   Joe,       i05,  93
+     *   5,   Joe,       i05,  93
      */
     csv::data data = csv::loadFile("tests/files/test.csv");
     std::vector<std::string> colnames = data.getColumnNames();
